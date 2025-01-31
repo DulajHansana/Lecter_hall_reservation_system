09e929d359dbc2de4bb2c4e68b466323924c7058
DuLa_1sT <161925626+DulajHansana@users.noreply.github.com>
2025-01-31 16:37:32 +0530

    Add initial project structure and resources for Lacture Hall Reservation System

---
+++ app/src/main/java/com/example/lacture_hall_reservation_system/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lacture_hall_reservation_system
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lacture_hall_reservation_system.ui.theme.Lacture_Hall_reservation_SystemTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lacture_Hall_reservation_SystemTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lacture_Hall_reservation_SystemTheme {
+        Greeting("Android")
+    }
+}
